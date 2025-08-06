a992800212c1a71e768240a4675afa61b098e4fb
Akshay P K <akshaypk.dev@gmail.com>
2025-08-06 13:03:45 +0530

    master

--- helper/helper.go
+++ helper/helper.go
@@ -3,60 +3,58 @@ package helper
 import (
 	// "Error"
 	"database/sql"
-	"lantorabde.app/models"
 	"fmt"
+
+	"lantorabde.app/models"
 )
 
 var DB *sql.DB
 
-func Insertuser(FullName string,Email string,Phone string,PasswordHash string,DrivingLicense string)(uint, error) {
+func Insertuser(FullName string, Email string, Phone string, PasswordHash string, DrivingLicense string) (uint, error) {
 	var id uint
 	query := `INSERT INTO billing (FullName,Email,Phone,PasswordHash,DrivingLicense,join_date,created_at,updated_at) 
               VALUES ($1, $2,$3,$4,$5,NOW(),NOW(),NOW()) RETURNING id`
-	err := DB.QueryRow(query,FullName,Email,Phone,PasswordHash,DrivingLicense).Scan(&id)
+	err := DB.QueryRow(query, FullName, Email, Phone, PasswordHash, DrivingLicense).Scan(&id)
 	if err != nil {
 		return 0, err
 	}
 	return id, nil
 }
 
-func GetUsers([]models.BDEUser, error){
-		rows, err := DB.Query(`
+func GetUsers() ([]models.BDEUser, error) {
+	rows, err := DB.Query(`
 			SELECT id,full_name,email,phone,password_hash,driving_license,role,join_date,status,created_at,updated_at  
 			FROM bde_users
 		`)
 
-		
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var usersList []models.BDEUser
+
+	for rows.Next() {
+		var user models.BDEUser
+		err := rows.Scan(
+			&user.FullName,
+			&user.Phone,
+			&user.Email,
+			&user.PasswordHash,
+			&user.DrivingLicense,
+			&user.Role,
+			&user.JoinDate,
+			&user.Status,
+			&user.CreatedAt,
+			&user.UpdatedAt,
+		)
 		if err != nil {
 			return nil, err
 		}
-		defer rows.Close()
-	
-		var usersList []models.BDEUser
-	
-		for rows.Next() {
-			var user models.BDEUser
-			err := rows.Scan(
-				&user.FullName,
-				&user.Phone,
-				&user.Email,
-				&user.PasswordHash,
-				&user.DrivingLicense,
-				&user.Role,
-				&user.JoinDate,      
-				&user.Status,         
-				&user.CreatedAt,      
-				&user.UpdatedAt, 
-
-			)
-			
-			usersList = append(usersList, user)
-		}
-	
-		fmt.Println("GetUsersSuccessful")
-	
-		return usersList, nil
+		usersList = append(usersList, user)
 	}
 
+	fmt.Println("GetUsersSuccessful")
 
-
+	return usersList, nil
+}
